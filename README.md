a8d85804ea305b29f3a733667dbce8b25f5a161a
Purnendu Dutta <purnendudutta521@gmail.com>
2025-03-30 14:41:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 3D-Portfolio-Website
+ It is a well-designed and fully functional portfolio website that is built with React.js and Three.js. It is a fully responsive website that works well on all devices.
---
+++ src/components/HomeInfo.jsx
@@ -0,0 +1,65 @@
+import { Link } from "react-router-dom";
+
+import { arrow } from "../assets/icons";
+
+const HomeInfo = ({ currentStage }) => {
+  if (currentStage === 1)
+    return (
+      <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
+        Hi, I'm
+        <span className='font-semibold mx-2 text-white'>Purnendu Dutta</span>
+        👋
+        <br />
+        My journey as a engineer started in a tier-3 college in India,<br/> where I started with Web Design.
+      </h1>
+    );
+
+  if (currentStage === 2) {
+    return (
+      <div className='info-box'>
+        <p className='font-medium sm:text-xl text-center'>
+        During graduation, I started learning programming languages,<br/> and picked up many skills along the way.
+        </p>
+
+        <Link to='/about' className='neo-brutalism-white neo-btn'>
+          Learn more
+          <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+        </Link>
+      </div>
+    );
+  }
+
+  if (currentStage === 3) {
+    return (
+      <div className='info-box'>
+        <p className='font-medium text-center sm:text-xl'>
+        I moved to Chennai(India) for my Master studies.
+        </p>
+
+        <Link to='/projects' className='neo-brutalism-white neo-btn'>
+          Visit my portfolio
+          <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+        </Link>
+      </div>
+    );
+  }
+
+  if (currentStage === 4) {
+    return (
+      <div className='info-box'>
+      <p className='font-medium sm:text-xl text-center'>
+      Want to connect? <br/> Feel free to follow me on 
+      <Link to="https://www.linkedin.com/in/purnendudutta/"> Linkedin</Link>
+      </p>
+      <Link to='/contact' className='neo-brutalism-white neo-btn'>
+        Let's talk.
+        <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+      </Link>
+    </div>
+    );
+  }
+
+  return null;
+};
+
+export default HomeInfo;
